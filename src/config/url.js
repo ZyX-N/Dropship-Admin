d6a8c62e8ec68476a41174f224ba5a40722afce9
Aman <aman.softfix@gmail.com>
2024-07-26 16:36:36 +0530

    url changes

--- src/config/url.js
+++ src/config/url.js
@@ -1,2 +1,2 @@
 // export const developmentUrl = "http://localhost:5000/api/admin"
-export const developmentUrl = "http://65.2.75.237:5000/api/admin"
+export const developmentUrl = "http://13.127.50.182:5000/api/admin"
