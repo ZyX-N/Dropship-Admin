fd49124869320ef50524f1040108cf34010bc74f
Aman <aman.softfix@gmail.com>
2024-11-04 18:58:36 +0530

    ur chnage

--- src/config/url.js
+++ src/config/url.js
@@ -1,2 +1,2 @@
-export const developmentUrl = "http://localhost:5000/api/admin"
-// export const developmentUrl = "http://13.127.50.182:5000/api/admin"
+// export const developmentUrl = "http://localhost:5000/api/admin"
+export const developmentUrl = "http://13.127.50.182:5000/api/admin"
